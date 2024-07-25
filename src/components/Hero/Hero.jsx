4ed0f21a3fe66f3d39762d7ec77e845c6f04758b
laauuugc <laauuugc@gmail.com>
2024-07-25 11:41:55 +0200

    centered hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,7 @@ import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return <section className={styles.container}>
-    <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I´m Laura</h1>
-        <p className={styles.description}>
-            I´m a full-stack developer with 2 years of experience.
-            Reach out if you´d like to learn more!
-        </p>
-        <a href="mailto:laauuugc@gmail.com" className={styles.contactBtn}>Contact Me</a>
-    </div>
-    <img src={getImageUrl("hero/circle.png")} 
+    <img src={getImageUrl("hero/name.png")} 
     alt="Hero image of me" 
     className={styles.heroImg}
     />
