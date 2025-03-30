d070be540e459c45101e29ba55c53be40b29a4f6
laauuugc <laauuugc@gmail.com>
2025-03-30 17:06:04 +0100

    Added skill carrousel and changed experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -10,18 +10,7 @@ export const Experience = () => {
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience</h2>
       <div className={styles.content}>
-        <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
-                <p>{skill.title}</p>
-              </div>
-            );
-          })}
-        </div>
+
         <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
@@ -31,7 +20,7 @@ export const Experience = () => {
                   alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <h3>{`${historyItem.role} - ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
@@ -43,6 +32,21 @@ export const Experience = () => {
             );
           })}
         </ul>
+
+        {/* Skill Carousel */}
+        <div className={styles.skillsWrapper}>
+          <div className={styles.skills}>
+            {skills.concat(skills).map((skill, id) => (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
       </div>
     </section>
   );
