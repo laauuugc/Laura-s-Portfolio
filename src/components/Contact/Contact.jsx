666864c05e1e61cb7aaa38940e014097cca02df0
laauuugc <laauuugc@gmail.com>
2025-03-30 11:15:27 +0100

    Responsive contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,11 +20,15 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/laura-gonzález-calleja-6445222a4/">linkedin.com/in/laura-gonzález-calleja</a>
+          <a href="https://www.linkedin.com/in/laura-gonzález-calleja-6445222a4/">Linkedin</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/laauuugc">https://github.com/laauuugc</a>
+          <a href="https://github.com/laauuugc">Github</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/cvIcon.png")} alt="CV icon" />
+          <a href={getImageUrl("contact/LAURA_GONZALEZ_CV.pdf")} download="LAURA_GONZALEZ_CV.pdf">My CV</a>
         </li>
       </ul>
     </footer>
