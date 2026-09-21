cd5be1346637cd479e1c87efad7800f27ab64a85
laauuugc <laauuugc@gmail.com>
2026-09-21 19:12:56 +0100

    Deleted cafe project

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,7 +17,7 @@ export const About = () => {
           { icon: "about/cursorIcon.png", title: "Chapter 1", 
             text: "Hey, I'm Laura. I'm a software engineer living in cloudy, too cloudy, Dublin. I could say coding is my passion but that's too cringe and I can only be so many cliches (guess whether I wear glasses or not)." },
           { icon: "about/serverIcon.png", title: "Chapter 2", 
-            text: "Despite the accent, colours and illustrations, I promise I'm serious. I've held jobs at different university organisations as well as being self-employed. At the same time, I've created several projects and participated in hackathons." },
+            text: "Despite the accent, colours and illustrations, I promise I'm serious. I've held jobs at different companies as well as being self-employed. At the same time, I've created several projects and participated in hackathons." },
           { icon: "about/uiIcon.png", title: "Chapter 3", 
             text: "I'm always on a quest to learn new tech. When I'm not on a laptop, you can find me painting whatever is in my mind, reading depressing books or cuddling my cat(Asia) while listening to Taylor Swift and drinking matcha." },
         ].map((item, index) => (
